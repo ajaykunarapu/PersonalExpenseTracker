9491d53b2902927cfc0c781b07d2fa9870f9983c
ajaykunarapu <163983245+ajaykunarapu@users.noreply.github.com>
2024-09-16 15:38:58 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import "./styles.css";
+import { Component } from "react";
+
+export default function App() {
+  return <div className="App"></div>;
+}
---
+++ src/component/LoginForm/index.js
@@ -0,0 +1,6 @@
+import "./index.css"
+component extends LoginForm{
+    <form>
+        <input type={Login}/>
+    </form>;
+}
